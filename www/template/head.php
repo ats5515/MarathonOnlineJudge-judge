29420f6a4d1dc6c1f25bd2ffcb5ce8b69ced8bc7
ats5515 <36256342+ats5515@users.noreply.github.com>
2020-03-21 09:11:24 +0900

    first commit

--- www/template/head.php
+++
@@ -1,7 +0,0 @@
-<meta charset="uft-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/css/uikit.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
-<link rel="stylesheet" type="text/css" href="./css/style.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
--- www/template/useapi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function run_cmd($cmd){
-	return shell_exec("sudo su -l ec2-user -c '" . $cmd . "'");
-}
-
-function run_cmd_exec($cmd, &$output, &$return_var){
-	return exec("sudo su -l ec2-user -c '" . $cmd . "'", $output, $return_var);
-}
