65aa43953f8c4d17618706437d5d9b8d6de54c21
ats5515 <36256342+ats5515@users.noreply.github.com>
2020-03-31 17:33:33 +0900

    update

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo chmod -R 777 /home/ec2-user/api/*
-sudo chmod -R 777 /home/ec2-user/judge/*
-sudo chmod 777 /home/ec2-user/www/*
-sudo chmod 777 /home/ec2-user/www/*/*
-sudo chmod 755 /home/ec2-user/www/
-sudo chmod 777 /home/ec2-user/submissions/
-sudo chmod 777 /home/ec2-user/submissions/*
+sudo chmod -R 777 ~/api/*
+sudo chmod -R 777 ~/judge/*
+sudo chmod 777 ~/www/*
+sudo chmod 777 ~/www/*/*
+sudo chmod 755 ~/www/
+sudo chmod 777 ~/submissions/
+sudo chmod 777 ~/submissions/*
